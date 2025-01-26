9ff08eafa086264820e20e20b0ada9cd75bdfb73
Ali Rhayem <alirhayem.1212@gmail.com>
2025-01-26 16:36:23 +0200

    Refactor FetchPullRequests command to remove dependency on GithubApiService in the constructor

--- app/Console/Commands/FetchPullRequests.php
+++ app/Console/Commands/FetchPullRequests.php
@@ -13,12 +13,10 @@ class FetchPullRequests extends Command
     protected $signature = 'pr:fetch';
     protected $description = 'Fetch pull request information from GitHub and write it to text files.';
 
-    protected GithubApiService $github;
 
-    public function __construct(GithubApiService $github)
+    public function __construct()
     {
         parent::__construct();
-        $this->github = $github;
     }
 
     public function handle()
